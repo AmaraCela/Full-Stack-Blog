753fdd2ace1a386bd7ee52622e1730ca0703cc68
AmaraCela <amaracela@gmail.com>
2024-02-06 09:50:03 +0100

    conversion to typescript

---
+++ server/dist/index.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require('express');
+const app = express();
+const port = 5000;
+app.get('/', (req, res) => {
+    res.send('Hello from Express');
+});
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});

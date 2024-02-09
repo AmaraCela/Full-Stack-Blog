e2b8dc6127d49b15857cf7cbdd2e0546ab80388b
AmaraCela <amaracela@gmail.com>
2024-02-09 10:25:35 +0100

    profile page responsive

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -17,24 +17,19 @@ const Profile = () => {
         {id:1,user:'User', image:laptop,profilePic:profile,title:"The title goes here...",description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dapibus eu lectus id gravida...',
         date:new Date(), tags:tags}
     ];
-    // type Id = {
-    //     id : string
-    // }
-    // const {id} = useParams<Id>();
     return ( 
-       <div className="flex w-full flex-row justify-between mt-8">
-        <BlogDisplayComponent blogs={blogs}/>
-            <div className="profile-card flex flex-row w-full ml-10 mr-10 h-fit items-center rounded-md sticky top-2 justify-between p-5  border-black border-double border-4">
-                <div className="flex justify-evenly flex-row">
-                <img src={profileImg} alt="" className="size-32" />
-                </div>
-                <div className="flex flex-col justify-evenly">
-                <h1 className="regular-font font-bold text-xl">Profile</h1>
-                <h1 className="regular-font text-xl">Username</h1>
-                <p className="regular-font text-xl">Number of posts : 3</p>
+       <div className="flex w-full flex-row justify-between mt-8 profile-content">
+        <div className="w-full flex justify-center">
+            <div className="profile-card flex flex-col w-full ml-10 mt-8 -mr-8 h-fit items-center rounded-md sticky top-2 justify-between p-5 border-black border-double border-4">
+                <img src={profileImg} alt="" className="size-32 profile-img" />
+                <div className="flex flex-col justify-evenly w-full mt-4 profile-info">
+                <h1 className="regular-font font-bold text-xl profile">Profile</h1>
+                <h1 className="regular-font text-xl username">Username</h1>
+                <p className="regular-font text-xl nr-posts">Number of posts : 3</p>
                 </div>
             </div>
-           
+        </div>
+        <BlogDisplayComponent blogs={blogs}/>   
        </div>
      );
 }

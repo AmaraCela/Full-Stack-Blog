d13a45455bbd3c4424d75924b676cec7d1ed8cfd
AmaraCela <amaracela@gmail.com>
2024-02-13 13:17:25 +0100

    pagination logic

--- client/src/components/Pagination.tsx
+++ client/src/components/Pagination.tsx
@@ -1,16 +1,178 @@
+import { useEffect, useState } from "react";
 import "../styles/pagination.css";
+
 const Pagination = () => {
-    return ( 
+    const nrPages = 4;
+    const [rows, setRows] = useState<JSX.Element []>([]);
+    const [activeIndex,setActiveIndex] = useState<number>(0);
+    const [pages, setPages] = useState<HTMLElement[]>([]);
+
+    const [leftArrow,setLeftArrow] = useState<HTMLElement|null>();
+    const [firstArrow, setFirstArrow] = useState<HTMLElement|null>();
+
+    useEffect(()=>{
+        console.log("here");
+        setPages(Array.from(document.querySelectorAll(".page")));
+        const left = document.querySelector(".previous-arrow") as HTMLElement;
+        const first = document.querySelector(".first-arrow") as HTMLElement;
+        if(left)
+        {
+            setLeftArrow(left);
+        }
+        if(first)
+        {
+            setFirstArrow(first);
+        }
+        leftArrowsInactive();
+    },[leftArrow,firstArrow]);
+
+
+    function leftArrowsInactive()
+    {
+        if(leftArrow)
+        {
+            leftArrow.style.cursor = "not-allowed";
+        }
+        if(firstArrow)
+        {
+            firstArrow.style.cursor = "not-allowed";
+        }
+    }
+
+    function leftArrowsActive()
+    {  
+        if(leftArrow)
+        {
+            leftArrow.style.cursor = "pointer";
+        }
+        if(firstArrow)
+        {
+            firstArrow.style.cursor = "pointer";
+        } 
+    }
+
+    function rightArrowsInactive()
+    {
+        const nextArrow:HTMLElement|null = document.querySelector(".next-arrow");
+        const lastArrow:HTMLElement|null = document.querySelector(".last-arrow");
+        if(nextArrow)
+        {
+            nextArrow.style.cursor = "not-allowed";
+        }
+        if(lastArrow)
+        {
+            lastArrow.style.cursor = "not-allowed";
+        }
+    }
+
+    function rightArrowsActive()
+    {
+        const nextArrow:HTMLElement|null = document.querySelector(".next-arrow");
+        console.log(nextArrow);
+        const lastArrow:HTMLElement|null = document.querySelector(".last-arrow");
+        if(nextArrow)
+        {
+            nextArrow.style.cursor = "pointer";
+        }
+        if(lastArrow)
+        {
+            lastArrow.style.cursor = "pointer";
+        }
+    }
+
+    function createPages()
+    {
+        if(nrPages>4)
+        {
+           rows.push(<div className="pagination-item page active" id="0"><p>{1}</p></div>)
+           rows.push(<div className="pagination-item page" id="1"><p>{2}</p></div>)
+           rows.push(<div className="pagination-item dots"><p>...</p></div>)
+           rows.push(<div className="pagination-item page" id={(nrPages-2).toString()}><p>{nrPages-1}</p></div>)
+           rows.push(<div className="pagination-item page" id={(nrPages-1).toString()}><p>{nrPages}</p></div>)
+        }
+        else
+        {
+            for(let i=0;i<nrPages;i++)
+            {
+                if(i==0)
+                {
+                    rows.push(<div className="pagination-item page active"><p>{i+1}</p></div>)   
+                }
+                else
+                {
+                    rows.push(<div className="pagination-item page"><p>{i+1}</p></div>)
+                }
+            }
+        }
+    }
+    createPages();
+ 
+
+    function movePage(direction : string):void
+    {
+        switch(direction)
+        {
+            case('right'):{
+                if(activeIndex<nrPages-1)
+                {
+                pages[activeIndex].classList.remove("active");
+                pages[activeIndex+1].classList.add("active");
+                if(activeIndex==nrPages-2)
+                {
+                  rightArrowsInactive(); 
+                }
+                setActiveIndex(activeIndex+1);
+                leftArrowsActive();
+                }
+                break;
+            }
+            case('last'):{
+                pages[activeIndex].classList.remove("active");
+                pages[nrPages-1].classList.add("active");
+                setActiveIndex(nrPages-1);
+                rightArrowsInactive();
+                leftArrowsActive();
+                break;
+            }
+            case("left"):
+            {
+                if(activeIndex > 0)
+                {
+                    pages[activeIndex].classList.remove("active");
+                    pages[activeIndex-1].classList.add("active");
+                    if((activeIndex-1)===0)
+                    {
+                        leftArrowsInactive();
+                    }
+                    rightArrowsActive();
+                    setActiveIndex(activeIndex-1);
+                }
+                break;
+            }
+            case('first'):
+            {
+                pages[activeIndex].classList.remove("active");
+                pages[0].classList.add("active");
+                setActiveIndex(0);
+                leftArrowsInactive();
+                rightArrowsActive();
+            }
+        }
+    }
+    return (
         <div className="flex regular-font mt-8 justify-center font-semibold">
-            <div className="pagination-item">
+            <div className="pagination-item first-arrow" title="first" onClick={()=>movePage("first")}>
+                <p>&#10094;&#10094;</p>
+            </div> 
+            <div className="pagination-item previous-arrow" title="previous" onClick={()=>movePage("left")}>
                 <p>&#10094;</p>
-            </div>
-            <div className="pagination-item"><p>1</p></div>
-            <div className="pagination-item"><p>2</p></div>
-            <div className="pagination-item"><p>...</p></div>
-            <div className="pagination-item"><p>10</p></div>
-            <div className="pagination-item">
+            </div>           
+            {rows}
+            <div className="pagination-item next-arrow" title="next" onClick={()=>movePage("right")}>
                 <p>&#10095;</p>
+            </div>
+            <div className="pagination-item last-arrow" title="last" onClick={()=>movePage("last")}>
+                <p>&#10095;&#10095;</p>
             </div>
         </div>
      );
--- client/src/pages/CreateBlog.tsx
+++ client/src/pages/CreateBlog.tsx
@@ -2,7 +2,7 @@ import "../styles/createBlog.css";
 const CreateBlog = () => {
     return ( 
         <div className="flex justify-center h-full items-center">
-            <div className="h-4/5 flex rounded-md w-3/4 bg-[#ffffff] create-div pl-4 pr-4">
+            <div className="flex rounded-md w-3/4 bg-[#ffffff] create-div pl-4 pr-4">
             <form className="grid blog-form align-middle w-full">
                 <label htmlFor="title" className="label regular-font">Title for new blog:</label>
                 <input type="text" id="title" className="input-format" />
@@ -14,7 +14,7 @@ const CreateBlog = () => {
                     <option value="tag2">tag2</option>
                 </select>
                 <input type="file" className="pt-4 regular-font label w-1/3 flex items-center file-input" multiple/>
-                <input type="submit" value="Submit" className="submit-bt-blog flex items-center justify-center regular-font font-semibold rounded-md text-xl border-solid border-black border-2 w-1/6 mb-4 cursor-pointer"/>
+                <input type="submit" value="Submit" className="submit-bt-blog flex items-center justify-center regular-font font-semibold rounded-md text-xl border-solid border-black border-2 w-fit h-fit mb-4 cursor-pointer p-1"/>
             </form>
             </div>
         </div>

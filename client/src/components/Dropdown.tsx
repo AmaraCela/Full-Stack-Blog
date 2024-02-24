8b685a051f4008dbdff0f7cc92df3ceaac38a775
AmaraCela <amaracela@gmail.com>
2024-02-24 20:02:59 +0100

    implemented logout

--- client/src/components/Dropdown.tsx
+++ client/src/components/Dropdown.tsx
@@ -1,15 +1,23 @@
-import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
-import { RootState } from "../store/store";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { AppDispatch, RootState } from "../store/store";
 import "../styles/dropdown.css";
+import { logoutUser } from "../store/auth/userSlice";
 
 const Dropdown = () => {
+    const dispatch = useDispatch<AppDispatch>();
+    const navigate = useNavigate();
     const userId = useSelector((state: RootState) => state.user.id);
+    
+    const handleClick = () => {
+        dispatch(logoutUser());
+        navigate('/login');
+    }
 
     return (
         <div className="absolute z-10 flex flex-col dropdown bg-white p-3 rounded-md">
             <Link to={`/profile/${userId}`} className="regular-font link">Profile</Link>
-            <Link to="/" className="regular-font mt-2 link">Logout</Link>
+            <button className="regular-font mt-2 link" onClick={handleClick}>Logout</button>
         </div>
     );
 }

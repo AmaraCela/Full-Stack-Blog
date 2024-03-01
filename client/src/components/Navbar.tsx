f8bbbcc35acc7f13799bcb540bedc2b51222eae6
AmaraCela <amaracela@gmail.com>
2024-03-01 08:37:36 +0100

    navbar container

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -14,7 +14,7 @@ const Navbar = () => {
     const isRootPath = location.pathname === '/';
 
     return (
-        <nav className="flex justify-between items-center nav flex-col mx-16">
+        <nav className="flex justify-between items-center nav flex-col mx-16 lg:container lg:mx-auto">
             <div className="flex justify-between w-full items-center">
                 <Link to="/"><h1 className="title">Chronicles</h1></Link>
                 {isAuthenticated ?

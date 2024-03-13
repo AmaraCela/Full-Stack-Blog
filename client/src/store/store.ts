7e1e55c3961c110e6012c9bed4544f227805248f
AmaraCela <amaracela@gmail.com>
2024-03-13 16:47:04 +0100

    persisting state

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -1,4 +1,4 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userReducer from "./auth/userSlice";
 import profileReducer from "./profile/profileSlice";
 import createBlogReducer from "./blog/blogSlice";
@@ -6,17 +6,27 @@ import passwordReducer from "./password/passwordSlice";
 import tagReducer from "./tag/tagSlice";
 import blogReducer from "./blog/blogSlice";
 import { useDispatch } from "react-redux";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+
+const persistConfig = {
+    key: 'root',
+    storage,
+}
+
+const rootReducer = combineReducers({
+    user: userReducer,
+    profile: profileReducer,
+    createBlog: createBlogReducer,
+    password: passwordReducer,
+    tag: tagReducer,
+    blog: blogReducer,
+})
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-    reducer: {
-        user: userReducer,
-        profile: profileReducer,
-        createBlog: createBlogReducer,
-        password: passwordReducer,
-        tag: tagReducer,
-        blog: blogReducer,
-
-    },
+    reducer: persistedReducer,
 });
 
 export type RootState = ReturnType<typeof store.getState>;
@@ -28,3 +38,4 @@ export const selectCreateBlog = (state: RootState) => state.createBlog;
 export const selectPassword = (state: RootState) => state.password;
 export const selectTag = (state: RootState) => state.tag;
 export const selectBlog = (state: RootState) => state.blog;
+export const persistor = persistStore(store);
